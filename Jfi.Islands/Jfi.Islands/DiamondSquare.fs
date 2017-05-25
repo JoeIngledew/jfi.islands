507e2741a26bc2a6559ad028f1c7843623aae1d4
Joe Ingledew <Joe.Ingledew@FASST.org.uk>
2017-05-25 14:30:14 +0100

    Diamond square algo maybe working?

--- Jfi.Islands/Jfi.Islands/DiamondSquare.fs
+++ Jfi.Islands/Jfi.Islands/DiamondSquare.fs
@@ -2,7 +2,7 @@
 
 open System
 
-type Point = int * int
+//type Point = int * int
 
 let rand = 
     function s -> if s = 0 then System.Random() else System.Random(s)
@@ -17,7 +17,10 @@ let randRangef (r : Random) rMin rMax =
     rMin + float32 (r.NextDouble()) * (rMax - rMin)
 
 let isPow2 (a : int) =
-    (a &&& (a - 1)) = 0
+    let fa = float a
+    let log = Math.Log(fa, float 2)
+    let pow = Math.Pow(float 2, Math.Round(log))
+    pow = fa
 
 let DsGrid size seed rMin rMax noise =
     let s = size - 1
@@ -34,59 +37,120 @@ let DsGrid size seed rMin rMax noise =
                 if ix = 0 || ix = s then
                     i |> Array.mapi (fun iix j -> if iix = 0 || iix = s then (randRangef rand rMin rMax) else j)
                 else i)
-        
-        // and some nasty mutable temp vars
-//        let mutable s0,s1,s2,s3,d0,d1,d2,d3,cn = 
-//            Unchecked.defaultof<float32>,
-//            Unchecked.defaultof<float32>,
-//            Unchecked.defaultof<float32>,
-//            Unchecked.defaultof<float32>,
-//            Unchecked.defaultof<float32>,
-//            Unchecked.defaultof<float32>,
-//            Unchecked.defaultof<float32>,
-//            Unchecked.defaultof<float32>,
-//            Unchecked.defaultof<float32>
 
+        let modi =
+            function i -> (rMax - rMin) * noise * (float32 i / float32 s)
         let mutable i = s
-        let is = seq { while i > 1 do yield i; i <- i/2 }
-        let mods = [ for i in is do yield (rMax - rMin) * noise * (float32 i / float32 s) ]
+        let is = [ while i > 1 do yield (i, (modi i)); i <- i/2 ]
+        printfn "%A" is
+        //let mods = [ for i in is do yield (rMax - rMin) * noise * (float32 i / float32 s) ]
 
         // Diamonds!
-        let rec innerLoop xCurr yCurr iterator (gridx : float32 [][])=
+        let rec innerLoopD xCurr yCurr iterator (gridx : float32 [][])=
+            let xP = if (xCurr + iterator) > s then (s-1) else (xCurr + iterator)
+            let yP = if (yCurr + iterator) > s then (s-1) else (yCurr + iterator)
             if xCurr < s then
                 let s0 = gridx.[xCurr].[yCurr]
-                let s1 = gridx.[xCurr + iterator].[yCurr]
-                let s2 = gridx.[xCurr].[yCurr + iterator]
-                let s3 = gridx.[xCurr + iterator].[yCurr + iterator]
+                let s1 = gridx.[xP].[yCurr]
+                let s2 = gridx.[xCurr].[yP]
+                let s3 = gridx.[xP].[yP]
                 let grid' =
                     gridx
                     |> Array.mapi (fun ix x -> 
                         if ix = (xCurr + (iterator / 2)) then
-                            x |> Array.mapi (fun iix y -> if iix = (yCurr + (iterator / 2)) then ((s0 + s1 + s2 + s3) / 4.0f) + (randRangef rand (0.0f - mods.[iterator]) mods.[iterator]) else y)
+                            x |> Array.mapi (fun iix y -> 
+                                if iix = (yCurr + (iterator / 2)) then 
+                                    ((s0 + s1 + s2 + s3) / 4.0f) 
+                                    + (randRangef rand (0.0f - (snd (is |> List.find (fun (i,_) -> i = iterator)))) (snd (is |> List.find (fun (i,_) -> i = iterator))))
+                                else y)
                         else x)
-                innerLoop (xCurr + 1) yCurr iterator grid'
+                innerLoopD (xCurr + 1) yCurr iterator grid'
             else gridx
 
         let rec loopD yCurr iterator gridd =
             if yCurr < s then
-                let grid' = innerLoop 0 yCurr iterator gridd
+                let grid' = innerLoopD 0 yCurr iterator gridd
                 loopD (yCurr + 1) iterator grid'
             else gridd
 
+        let rec innerLoopS xCurr yCurr iterator (gridsx : float32 [][]) =
+            let xP = if (xCurr + iterator) > s then (s-1) else (xCurr + iterator)
+            let yP = if (yCurr + iterator) > s then (s-1) else (yCurr + iterator)
+            let xC = if (xCurr + (iterator / 2)) > s then (s-1) else (xCurr + (iterator / 2))
+            let yC = if (yCurr + (iterator / 2)) > s then (s-1) else (yCurr + (iterator / 2))
+            let xNC = if (xCurr - (iterator / 2)) < 0 then 0 else (xCurr - (iterator / 2))
+            let yNC = if (yCurr - (iterator / 2)) < 0 then 0 else (yCurr - (iterator / 2))
+            let xW = if (xCurr + iterator + (iterator / 2)) > s then (s - 1) else (xCurr + iterator + (iterator / 2)) 
+            let yW = if (yCurr + iterator + (iterator / 2)) > s then (s - 1) else (yCurr + iterator + (iterator / 2)) 
+            if xCurr < s then 
+                let s0 = gridsx.[xCurr].[yCurr]
+                let s1 = gridsx.[xP].[yCurr]
+                let s2 = gridsx.[xCurr].[yP]
+                let s3 = gridsx.[xP].[yP]
+                let cn = gridsx.[xC].[yC]
+                let d0 = if yCurr <= 0 then (s0 + s1 + cn) / 3.0f else (s0 + s1 + cn + gridsx.[xC].[yNC]) / 4.0f
+                let d1 = if xCurr <= 0 then (s0 + cn + s2) / 3.0f else (s0 + cn + s2 + gridsx.[xNC].[yC]) / 4.0f
+                let d2 = if xCurr >= (s - iterator) then (s1 + cn + s3) / 3.0f else (s1 + cn + s3 + gridsx.[xW].[yC]) / 4.0f
+                let d3 = if yCurr >= (s - iterator) then (cn + s2 + s3) / 3.0f else (cn + s2 + s3 + gridsx.[xC].[yW]) / 4.0f
+                let gridsx' =
+                    gridsx
+                    |> Array.mapi (fun ix x -> 
+                        if ix = (xCurr + (iterator / 2)) then
+                            x |> Array.mapi (fun iix y -> 
+                                if iix = (yCurr + iterator) then 
+                                    d0 + (randRangef rand (0.0f - (snd (is |> List.find (fun (i,_) -> i = iterator)))) (snd (is |> List.find (fun (i,_) -> i = iterator))))
+                                else d1 + (randRangef rand (0.0f - (snd (is |> List.find (fun (i,_) -> i = iterator)))) (snd (is |> List.find (fun (i,_) -> i = iterator)))))
+                        elif ix = (xCurr + iterator) then 
+                            x |> Array.mapi (fun iix y -> 
+                                if iix = (yCurr + (iterator / 2)) then 
+                                    d2 + (randRangef rand (0.0f - (snd (is |> List.find (fun (i,_) -> i = iterator)))) (snd (is |> List.find (fun (i,_) -> i = iterator))))
+                                else y)
+                        else 
+                            x |> Array.mapi (fun iix y -> 
+                                if iix = (yCurr + (iterator / 2)) then 
+                                    d1 + (randRangef rand (0.0f - (snd (is |> List.find (fun (i,_) -> i = iterator)))) (snd (is |> List.find (fun (i,_) -> i = iterator))))
+                                else y))
+                innerLoopS (xCurr + 1) yCurr iterator gridsx'
+            else gridsx
+
+        let rec loopS yCurr iterator grids =
+            if yCurr < s then
+                let grid' = innerLoopS 0 yCurr iterator grids
+                loopS (yCurr + 1) iterator grid'
+            else grids
+            
+
+        let rec outerLoop acc grido =
+            printfn "acc %A grid %A" acc grido
+            match acc with 
+            | [] -> grido
+            | h::t -> 
+                let grido' = loopD 0 (fst h) grido
+                let grido'' = loopS 0 (fst h) grido'
+                outerLoop t grido''
+
+        let res = outerLoop (is |> List.rev) grid
+        let min = res |> Array.minBy (fun l -> l |> Array.min) |> Array.min
+        let max = res |> Array.maxBy (fun l -> l |> Array.max) |> Array.max
+        let normalise = 
+            function n -> 
+                        let n' = if min < 0.0f then n - min else n
+                        let nf = Math.Floor(float ((n' / 256.0f) * max))
+                        if nf > 1.0 then nf - 1.0 else nf
+
+        let normalised = res |> Array.map (fun l -> l |> Array.map (fun i -> normalise i))
+        Some normalised
+
         // see https://github.com/eogas/DiamondSquare/blob/master/DiamondSquare/DiamondSquare/DiamondSquare.cs
-        // just so it's valid...
-        let a = "b"
-        Some a
-        
 
-let testArr = [1;2;3;]
-
-let testArr' = testArr |> List.mapi (fun ix i -> if ix = 1 then 5 else i) // modifying lists!
-
-type Square = {
-    position : Point
-    tl : double // height @ topleft
-    tr : double
-    bl : double
-    br : double }
+//let testArr = [1;2;3;]
+//
+//let testArr' = testArr |> List.mapi (fun ix i -> if ix = 1 then 5 else i) // modifying lists!
+//
+//type Square = {
+//    position : Point
+//    tl : double // height @ topleft
+//    tr : double
+//    bl : double
+//    br : double }
 
--- Jfi.Islands/Jfi.Islands/Generator.fs
+++ Jfi.Islands/Jfi.Islands/Generator.fs
@@ -1,2 +1,13 @@
-﻿module Generator
+﻿module Generator 
 
+//let roughness = 4
+//let gridSize = 257
+//
+//let waterThreshold = 35
+//let sandThreshold = 44
+//let grassThreshold = 84
+//let stoneThreshold = 94
+
+let gen size seed =
+    let ds = DiamondSquare.DsGrid size seed 0.0f 100.0f (4.0f / 10.0f)
+    ds
